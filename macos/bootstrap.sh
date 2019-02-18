5dbdbb85eda3b053fe32887d06733c9e5b5097a4
Peter Mounce <petermounce@improbable.io>
2019-02-18 10:02:59 +0100

    runs, but cannot compete yadr rake install

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -8,26 +8,6 @@ sudo easy_install pip
 sudo pip install pip --upgrade
 sudo pip install ansible==2.7.5
 ansible-galaxy install --role-file requirements.yml --roles-path roles
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# brew install readline xz
-# curl https://pyenv.run | bash
-# cp dot-bashrc "${HOME}/.bashrc"
-# . "${HOME}/.bashrc"
-# pyenv install 2.7.15
-# CFLAGS="-I$(brew --prefix openssl)/include" \
-# LDFLAGS="-L$(brew --prefix openssl)/lib" \
-# pyenv install -v 3.7.1
-
-# mkdir "${HOME}/.ssh"
-# ln -s "${HOME}/Dropbox/profiles/dot-ssh" "${HOME}/dot-ssh"
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh)"
-# rm -rf "${HOME}/.zsh.after" "${HOME}/.zsh.before" "${HOME}/.zsh.prompts"
-# ln -s "${HOME}/Dropbox/profiles/osx/.zsh.after" "${HOME}/"
-# ln -s "${HOME}/Dropbox/profiles/osx/.zsh.before" "${HOME}/"
-# ln -s "${HOME}/Dropbox/profiles/osx/.zsh.prompts" "${HOME}/"
-# pushd .yadr
-#   git checkout petemounce
-# popd
 
 
 # http://krypted.com/mac-security/mac-setting-screen-saver-from-the-cli/
