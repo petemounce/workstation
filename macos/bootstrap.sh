6f053105cf40b812532cc75fc2e431c4b273cb60
Peter Mounce <petermounce@improbable.io>
2019-10-18 14:24:35 +0100

    fixes

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -2,17 +2,22 @@
 set -eu -o pipefail
 set -x
 
-xcode-select --install
+xcode-select --install || echo "xcode-select has been installed already, probably."
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 sudo easy_install pip
 sudo pip install pip --upgrade
-sudo pip install ansible==2.7.5
+sudo pip install ansible==2.8.6
 ansible-galaxy install --role-file requirements.yml --roles-path roles
 
 
 # http://krypted.com/mac-security/mac-setting-screen-saver-from-the-cli/
 echo "Install Aerial Screensaver: https://github.com/JohnCoates/Aerial"
-echo "Configure screensaver"
+curl -LlJ https://github.com/JohnCoates/Aerial/releases/download/v1.6.2/Aerial.saver.zip -o Aerial.saver.zip
+unzip Aerial.saver.zip
+open Aerial.saver
+echo "any key to continue"
+read -r
+rm -rf "Aerial*"
 echo "Configure lock screen 2 minutes"
 defaults -currentHost write com.apple.screensaver idleTime 120
 
