2222ec32dea876382ba6492ec1b9eebeccb6e01d
Peter Mounce <petermounce@improbable.io>
2020-08-18 10:06:11 +0100

    Aerials to ansible

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -9,15 +9,3 @@ sudo easy_install pip
 sudo pip install pip --upgrade
 sudo pip install "ansible==${ANSIBLE_VERSION}"
 ansible-galaxy install --role-file "requirements.yml" --roles-path "roles-external"
-
-# http://krypted.com/mac-security/mac-setting-screen-saver-from-the-cli/
-echo "Install Aerial Screensaver: https://github.com/JohnCoates/Aerial"
-curl -LlJ https://github.com/JohnCoates/Aerial/releases/download/v1.9.2/Aerial.saver.zip -o Aerial.saver.zip
-unzip Aerial.saver.zip
-open Aerial.saver
-echo "any key to continue"
-read -r
-rm -rf "Aerial*"
-echo "Configure lock screen 2 minutes"
-defaults -currentHost write com.apple.screensaver idleTime 120
-
