0db151a61e6ce7882c073c2561f764a09ffd06fc
Peter Mounce <pete@neverrunwithscissors.com>
2024-05-16 12:04:09 +0100

    fix

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -6,6 +6,7 @@ UV_VERSION="0.1.44"
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/${UV_VERSION}/uv-installer.sh | sh
 #xcode-select --install || echo "xcode-select has been installed already, probably."
 #sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.15.pkg -target /
+export PATH="${HOME}/.cargo/bin:${PATH}"
 uv venv
 source ".venv/bin/activate"
 ANSIBLE_VERSION="${1:-2.9.11}"
